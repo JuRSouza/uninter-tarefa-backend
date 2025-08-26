ddcf0d043106ca675f303fe8c594f7436533a5e2
Juliana Souza <121642110+JuRSouza@users.noreply.github.com>
2025-08-26 17:32:20 -0300

    feat: implementar TarefaController com endpoints REST (CRUD)

--- uninter-backend/src/main/java/br/com/uninter/uninter_backend/controller/TarefaController.java
+++ uninter-backend/src/main/java/br/com/uninter/uninter_backend/controller/TarefaController.java
@@ -16,18 +16,34 @@ public class TarefaController {
     private TarefaRepository repository;
 
     @PostMapping
-    public Tarefa criarTarefa(@RequestBody Tarefa tarefa){
+    public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
         return repository.save(tarefa);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Tarefa> buscarTarefa(@PathVariable Long id){
+    public ResponseEntity<Tarefa> buscarTarefa(@PathVariable Long id) {
         Optional<Tarefa> tarefa = repository.findById(id);
         return tarefa.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefa> atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaAtualizada) {
+        return repository.findById(id).map(tarefa -> {
+            tarefa.setNome(tarefaAtualizada.getNome());
+            tarefa.setDataEntrega(tarefaAtualizada.getDataEntrega());
+            tarefa.setResponsavel(tarefaAtualizada.getResponsavel());
+            repository.save(tarefa);
+            return ResponseEntity.ok(tarefa);
+        }).orElse(ResponseEntity.notFound().build());
+    }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTarefa(@PathVariable Long id){
+        return repository.findById(id).map(tarefa -> {
+            repository.delete(tarefa);
+            return ResponseEntity.ok().<Void>build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
 
 }
